8243f61bddba9daeeca62e0a6b60c09b68087786
Vsein <vseinssj@gmail.com>
2023-05-21 07:53:56 +0300

    Add glossary page without api requests

--- frontend/src/pages/Glossary/index.jsx
+++ frontend/src/pages/Glossary/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Icon from '@mdi/react';
-import {
-  mdiMagnify,
-  mdiTune,
-  mdiForest,
-  mdiBookOpenBlankVariant,
-} from '@mdi/js';
+import { mdiMagnify } from '@mdi/js';
 import useTitle from '../../hooks/useTitle';
 
 export default function Glossary() {
@@ -16,12 +11,45 @@ export default function Glossary() {
     <main className="glossary-page">
       <div className="glossary-search">
         <div className="glossary-searchbar">
-          <input className="searchbar-input" />
+          <button className="searchbar-filter-button">
+            <img src="../../assets/filter.png" className="filter-img" />
+          </button>
+          <input className="searchbar-input"></input>
           <button className="searchbar-button">
             <Icon path={mdiMagnify} className="icon searchbar-icon" />
           </button>
         </div>
-        <div className="glossary-searchresults"></div>
+        <div className="glossary-filters">
+          <h3>Типы почв</h3>
+          <h3>Ареалы</h3>
+          <h3>Регионы</h3>
+          <h3>Климат</h3>
+          <h3>Активные в-ва</h3>
+          <h3>Группы лекарств</h3>
+        </div>
+        <div className="glossary-searchresults">
+          <div className="searchresult-card">
+            <div className="img-icon"/>
+            <div className="plant-label">
+              <p>Name</p>
+            </div>
+          </div>
+
+          <div className="searchresult-card">
+            <div className="img-icon"/>
+            <div className="plant-label">
+              <p>Name</p>
+            </div>
+          </div>
+
+          <div className="searchresult-card">
+            <div className="img-icon"/>
+            <div className="plant-label">
+              <p>Name</p>
+            </div>
+          </div>
+
+        </div>
       </div>
       <div className="glossary-container">
         <div className="glossary-details">
@@ -35,6 +63,7 @@ export default function Glossary() {
               </div>
               <div className="glossary-char-text">куст</div>
             </div>
+            <div />
             <div className="glossary-char">
               <div className="glossary-icon">
                 <img
@@ -44,6 +73,7 @@ export default function Glossary() {
               </div>
               <div className="glossary-char-text">апрель</div>
             </div>
+            <div />
             <div className="glossary-char">
               <div className="glossary-icon"></div>
               <div className="glossary-char-text">35%</div>
@@ -52,22 +82,18 @@ export default function Glossary() {
           <div className="glossary-description">
             <h2 className="glossary-description-text2">
               Аир обыкновенный
-              <em style={{color: 'rgba(255, 255, 255, 0.5)'}}>
+              <em style={{ color: 'rgba(255, 255, 255, 0.5)' }}>
                 (Acorus calamus L.)
               </em>
             </h2>
-            <h3 className="glossary-description-text">
-              Обитание
-            </h3>
-            <hr/>
-            <h3 className="glossary-description-text">
-              Посадка
-            </h3>
-            <hr/>
+            <h3 className="glossary-description-text">Обитание</h3>
+            <hr />
+            <h3 className="glossary-description-text">Посадка</h3>
+            <hr />
             <h3 className="glossary-description-text">
               Химический состав и применение
             </h3>
-            <hr/>
+            <hr />
           </div>
         </div>
       </div>
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { plantApi } from './plant';
 
 const store = configureStore({
   reducer: {
+    [plantApi.reducerPath]: plantApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      plantApi.middleware,
+    ]),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
